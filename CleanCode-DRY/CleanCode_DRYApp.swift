98480d11c9ff44db7af3b8e899a06416e6d5ecc6
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-07 13:07:58 +0200

    Initial Commit

---
+++ CleanCode-DRY/CleanCode_DRYApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCode_DRYApp.swift
+//  CleanCode-DRY
+//
+//  Created by Eric on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanCode_DRYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
